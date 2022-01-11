3288cc28ec1e9d3da35d317d0adc1e4b45126da9
swayam_sidd <devswayam@ingenuitas.com>
2022-01-11 20:27:27 +0530

    commit #2

--- myapp/settings.py
+++ myapp/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
