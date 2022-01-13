d0016e28e2bc4a17cf54da65656da7ec5c764d84
swayam_sidd <devswayam@ingenuitas.com>
2022-01-14 01:02:32 +0530

    Docker-PSQL push

--- myapp/settings.py
+++ myapp/settings.py
@@ -1,7 +1,11 @@
 from pathlib import Path
 import os
 from decouple import config
+# import environ
 
+
+# env = environ.Env()
+# environ.Env.read_env()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -9,7 +13,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -21,7 +25,6 @@ INSTALLED_APPS = [
     'tradebook',
     'rest_framework',
     'bootstrap_datepicker_plus',
-    
 ]
 
 MIDDLEWARE = [
@@ -55,11 +58,34 @@ TEMPLATES = [
 WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': 'localhost',
+            'PORT': 5432,
+        }
 }
+# if 'RDS_DB_NAME' in os.environ:
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': os.environ.get('POSTGRES_NAME'),
+#             'USER': os.environ.get('POSTGRES_USER'),
+#             'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+#             'HOST': 'localhost',
+#             'PORT': 5432,
+#         }
+#     }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
